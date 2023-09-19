47b40b62b07beae466e10a0ee30a7abf287fc58c
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-09-19 16:03:19 +0300

    Delete some express generator files

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/new', indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
